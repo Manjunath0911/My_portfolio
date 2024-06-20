f1185ff6037d68a2bc590a0130bc6520d95cf89b
Manjunath <142318269+Manjunath0911@users.noreply.github.com>
2024-06-20 23:15:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+/*...................toggle................. */
+
+
+let menuIcon =  document.querySelector('#menu-icon');
+let navbar =  document.querySelector('.navbar')
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+
+}
+
+/*...................scroll selection active link................. */
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach.apply(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+
+            });
+        };
+    });
+/*...................sticky nav bar................. */
+
+let header = document.querySelector('header');
+header.classList.toggle('sticky', window.scrollY > 100);
+
+
+/*...................toggle................. */
+
+
+menuIcon.classList.remove('fa-xmark');
+navbar.classList.remove('active');
+
+};
+
+
+ScrollReveal({ 
+    distance: '30px',
+    duration: 1500,
+    delay: 200,
+ });
+
+ ScrollReveal().reveal('.home-content, heading', {origin: 'top '});
+ ScrollReveal().reveal('.home-img, services-container, .portfolio-box, .footer', {origin: 'buttom '});
+ ScrollReveal().reveal('.home-content h3, .about-img', {origin: 'left '});
+ ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right '});
+
+ /*......................typed.js................ */
+
+ const typed = new Typed('.multiple-text', {
+    strings: ['Python Developer', 'RPA Developer','Web Designer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+ });
